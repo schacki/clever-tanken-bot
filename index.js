9742f3c297cd5659a5699716c434b0af5e2b69a6
Vostro162 <v162-gitkraken@etlgr.com>
2018-01-19 17:01:57 +0100

    send json for debuging

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-const { TelegramBot } = require('bottender');
-const { createServer } = require('bottender/express');
+const { TelegramBot } = require('bottender')
+const { createServer } = require('bottender/express')
 
-const config = require('./bottender.config.js');
+const config = require('./bottender.config.js')
 
 const bot = new TelegramBot({
   accessToken: config.telegram.accessToken,
 });
 
 bot.onEvent(async context => {
-  await context.sendText('Hello World')
+  await context.sendText(JSON.stringify(context))
 });
 
-const server = createServer(bot);
+const server = createServer(bot)
 
 server.listen(process.env.PORT, () => {
-  console.log("server is running on" + process.env.PORT + " port...");
+  console.log("server is running on" + process.env.PORT + " port...")
 });
