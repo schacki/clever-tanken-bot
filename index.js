609285d0284646ba235c917fc67f41f9b0a3eb35
Vostro162 <v162-gitkraken@etlgr.com>
2018-01-19 13:21:28 +0100

    change port

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ bot.onEvent(async context => {
 
 const server = createServer(bot);
 
-server.listen(5000, () => {
-  console.log('server is running on 5000 port...');
+server.listen(8443, () => {
+  console.log('server is running on 8443 port...');
 });
