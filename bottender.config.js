7b95f7f125d45bfa5e208cfbefe25618c91aa21d
Vostro162 <v162-gitkraken@etlgr.com>
2018-01-19 12:14:13 +0100

    bottender init

---
+++ bottender.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  telegram: {
+    accessToken: '__PUT_YOUR_ACCESS_TOKEN_HERE__'
+  }
+};
---
+++ index.js
@@ -0,0 +1,18 @@
+const { TelegramBot } = require('bottender');
+const { createServer } = require('bottender/express');
+
+const config = require('./bottender.config.js').telegram;
+
+const bot = new TelegramBot({
+  accessToken: config.accessToken,
+});
+
+bot.onEvent(async context => {
+  await context.sendText('Hello World');
+});
+
+const server = createServer(bot);
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
