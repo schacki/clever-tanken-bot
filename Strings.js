834e5cb4c872a915e4823585b544f3f767c4dd51
Vostro162 <v162-gitkraken@etlgr.com>
2018-01-21 16:47:23 +0100

    implemented function get fuel station prices by city

---
+++ Strings.js
@@ -0,0 +1,25 @@
+exports.welcomeText = '<p><prosody volume="x-loud" pitch="+6%">Willkommen bei Clever-Tanken.</prosody></p><p> Ich kann dir sagen wie hoch die Benzinpreise in deiner Stadt sind.</p>';
+exports.helpText = 'Frag, "Wie teuer ist Super E5 in München?" und ich finde für dich heraus wie hoch der Super E5 Preis in München aktuell ist. Du kannst natürlich auch nach anderen Spritsorten oder Städten fragen.';
+exports.UNKNOWN_TEXT = 'Leider konnte ich keine Ergebenisse finden.';
+exports.UNKNOWN_FUELTYPE_TEXT = "Tut mir leid, ich kenne die Spritsorte nicht.";
+exports.UNKNOWN_CITY_TEXT = "Tut mir leid, ich kenne die Stadt nicht.";
+
+exports.NO_PRICE_FOUND_TEXT = function(fuelType, city) {
+  return 'Ich habe keinen Preis für ' + fuelType + ' in ' + city + ' gefunden.';
+}
+
+exports.UNKNOWN_FUELTYPE_TEXT = function(fuelType) {
+  return 'Leider kenne ich die Spritsorte ' + fuelType + ' nicht.';
+}
+
+exports.UNKNOWN_CITY_TEXT = function(city) {
+  return 'Leider kenne ich die Stadt ' + city + ' nicht.';
+}
+
+exports.resultTextMinMaxPrice = function(fuelType, city, minPrice, maxPrice) {
+  return fuelType + ' kostet aktuell in ' + city + ', zwischen ' + minPrice +' Euro , und ' + maxPrice + ' Euro pro Liter.';
+}
+
+exports.resultTextPrice = function(fuelType, city, price) {
+  return 'Der Preis für ' +  fuelType + ' in ' + city + ' kostet ' + price + ' Euro';
+}
--- index.js
+++ index.js
@@ -1,20 +1,106 @@
 
 const { TelegramBot } = require('bottender')
 const { createServer } = require('bottender/express')
+const { Strings } = require('./Strings.js');
 const { HTTPCleverTankenProvider } = require('./HTTPCleverTankenProvider.js');
+const {Wit, log} = require('node-wit');
 
 const config = require('./bottender.config.js')
 
+const witClient = new Wit({
+  accessToken: "XEIUUVCOS5OKC2MRNCKKMEQI6XTB3FFR",
+  logger: new log.Logger(log.DEBUG) // optional
+});
+
+
+// Bot
 const bot = new TelegramBot({
   accessToken: config.telegram.accessToken,
 });
-
 bot.onEvent(async context => {
-  await context.sendText(JSON.stringify(context))
+
+	if(context.event.isText) {
+		handleText(context.event.text)
+	}
+ 	
 });
 
-const server = createServer(bot)
 
+// Start the Server
+const server = createServer(bot)
 server.listen(process.env.PORT, () => {
   console.log("server is running on" + process.env.PORT + " port...")
 });
+
+// Functions
+
+function fetchTextInfo(text) {
+	return witClient.message(text)
+}
+
+function handleText(text, context) {
+
+  	let result = fetchTextInfo(text)
+  
+  	let fuelType = result.entities.fuelType
+	let location = result.entities.location
+	if(fuelType && location) {
+		fetchFuelStationPrice(location, fuelType, context)
+	}
+	
+}
+
+function fetchFuelStationPrice(city, fuel, context) {
+
+	
+	context.typing(3000)
+		
+ 	HTTPCleverTankenProvider.getFuelStationPrices(city, fuel, function(isOK, fuelStation) {
+
+          if(isOK) {
+
+              var min = Number(fuelStation.price_min);
+              var max = Number(fuelStation.price_max);
+
+              min = min.toString();
+              min = min.slice(0, (min.indexOf("."))+3);
+              min = Number(min);
+
+              max = max.toString();
+              max = max.slice(0, (max.indexOf("."))+3);
+              max = Number(max);
+
+              if(min == max) {
+              	context.sendText(Strings.resultTextPrice(fuelStation.fueltype, fuelStation.city, max))
+              } else {
+                context.sendText(Strings.resultTextMinMaxPrice(fuelStation.fueltype, fuelStation.city, min, max))
+              }
+
+          } else {
+
+              if(fuelStation.reason == "UNKNOWN_FUELTYPE") {
+
+                if(fuel == undefined) {
+                	context.sendText(Strings.UNKNOWN_FUELTYPE_TEXT)
+                } else {
+                	context.sendText(Strings.UNKNOWN_FUELTYPE_TEXT(fuel))
+                }
+
+              } else if(fuelStation.reason == "UNKNOWN_CITY") {
+
+                if(city == undefined) {
+                	context.sendText(Strings.UNKNOWN_CITY_TEXT)
+                } else {
+                	context.sendText(Strings.UNKNOWN_CITY_TEXT(city))
+                }
+
+              } else if(fuelStation.reason == "NO_PRICE_FOUND") {
+              	 context.sendText(Strings.NO_PRICE_FOUND_TEXT(fuel, city))
+              } else {
+              	 context.sendText(Strings.UNKNOWN_TEXT)
+              }
+
+          }
+
+      })
+}
