00ae6741ba951db6a2e587a5866ef965580c65c6
MSwagHartig <37694304+MSwagHartig@users.noreply.github.com>
2018-03-23 13:14:29 +0100

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -1,10 +1,15 @@
 const { TelegramBot, FileSessionStore } = require("bottender")
 const { Telegram, Messenger } = require("./config.json")
 
-// Telegram Bot
-const bot = new TelegramBot({
-  accessToken: Telegram.accessToken,
-  sessionStore: new FileSessionStore()
-})
+let bot;
+const arg = JSON.parse(process.env.npm_config_argv)
+if(arg.original.length > 1 && arg.original[0] == "run" &&  arg.original[1] == "dev") {
+    bot = new ConsoleBot()
+} else {
+    bot = new TelegramBot({
+        accessToken: Telegram.accessToken,
+        sessionStore: new FileSessionStore()
+     })
+}
 
 exports.Bot = bot
